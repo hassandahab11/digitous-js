bff13250b50bb4d95f7484462bd2d5c1eaff16d3
unknown <hassandahab11@gmail.com>
2020-12-07 16:41:00 +0100

    my primary version of test js

---
+++ file.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+//String
+
+var myNmaeIs = "xxx";
+console.log(myNmaeIs);
+myNmaeIs = "Hassan";
+console.log(myNmaeIs);
+
+//Concatenation
+
+var name = "hassan";
+console.log("Nice to meet you" + " "+ name);
+
+
+//String Lengeth
+
+var testLength = " I'm very long !";
+console.log(testLength.length);
+
