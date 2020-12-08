975a8850cb826f30ab758a5319ce8a394a464232
unknown <hassandahab11@gmail.com>
2020-12-08 13:56:58 +0100

    new version js test

---
+++ jour01.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+
+//String
+
+var myNmaeIs = "xxx";
+console.log(myNmaeIs);
+myNmaeIs = "Hassan";
+console.log(myNmaeIs);
+
+//Concatenation
+
+var name = "hassan";
+console.log("Nice to meet you" + " "+ name);
+
+
+//String Lengeth
+
+var testLength = " I'm very long !";
+console.log(testLength.length);
+
+//Replace
+var food ="croissant is meh";
+console.log(food.replace("meh","so good"));
+
+//Up and Down
+var x = "This Is Cool";
+var xToUppercase = x.toUpperCase();
+var xToDown = x.toLowerCase();
+console.log(x);
+console.log(xToDown);
+console.log(xToUppercase);
+
+
+//Split
+
+var word = "banana";
+var letters = word.split("");
+console.log(letters);
+
+//Template
+
+var age ="xx";
+age = 34;
+console.log(`I'm ${age} Years old`);
+var xx ="34";
+console.log(`I'm ${xx} Years old`);
