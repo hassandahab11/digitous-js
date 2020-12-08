49a7716c951fca6877934e6d61342e6585dc1b32
unknown <hassandahab11@gmail.com>
2020-12-08 14:23:29 +0100

    my primary version of test jour02

---
+++ jour02.js
@@ -0,0 +1,17 @@
+// 01 Number
+
+var x = 102;
+var y = "13.9";
+console.log(x,y);
+
+//03 Round
+
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
+
+
+//04 Arithmétique
+var test = 12;
+var bis = 5;
+console.log(test + bis, test/ bis, test * bis, test % bis, test- bis);
