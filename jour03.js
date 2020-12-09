36d2c12950e242d54e18d0d3339c1291378bbc1c
unknown <hassandahab11@gmail.com>
2020-12-09 15:03:33 +0100

    my primary version of test J-3

---
+++ jour03.js
@@ -0,0 +1,23 @@
+// 01 Array
+var fruits = ["Mango", "Lemon", "Blueberry"];
+console.log(fruits);
+console.table(fruits);
+
+// 02 Access
+
+var ingerdients = ["eggs", "milk", "butter"];
+console.log(ingerdients[1]);
+console.log(ingerdients[2]);
+
+// 03 Add and Remove
+
+var librairie = ["pen", "book", "lamp"];
+console.log(librairie);
+console.log(librairie.unshift("chair"));
+console.log(librairie);
+console.log(librairie.pop());
+console.log(librairie);
+console.log(librairie.push("laptop"));
+console.log(librairie);
+console.log(librairie.shift());
+console.log(librairie);
