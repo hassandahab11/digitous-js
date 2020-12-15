4446e6487262bf73e9ae84afb76bd2bfe399fb97
unknown <hassandahab11@gmail.com>
2020-12-15 15:12:07 +0100

    second version of j5 tst

--- jour05-exoX.js
+++ jour05-exoX.js
@@ -1,10 +1,11 @@
-function calculate(num1, num2){
-    console.log(num1 * num2);
-}
-if(process.argv.length ===4){
 
-// console.log(process.argv);
-calculate(process.argv[2], process.argv[3]);
-}else{
-    console.log("Error");
+function calculate (num1,operateur, num2 ){
+    var x = parseInt(num1);
+    var y = parseInt(num2);
+    if(operateur ==="+"){
+        return x + y;
+    }
+
 }
+ console.log(calculate(process.argv[2], process.argv[3],process.argv[4])); 
+
