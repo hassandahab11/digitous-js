feeeaf4844881297ff919d1de021e94802b038fe
unknown <hassandahab11@gmail.com>
2020-12-16 16:20:33 +0100

    second vrsn of j6 tst

--- jour-06.js
+++ jour-06.js
@@ -1,5 +1,6 @@
-function formatDate(){
+function formatDate(aaaa, mm, jj){
     var dateX = new Date();
-console.log(`${dateX.getMonth()} ${dateX.getDate()} ${dateX.getFullYear()}`);
+console.log(`${dateX.getDay(jj)} ${dateX.getMonth(mm)} ${dateX.getFullYear(aaaa)} ` );
 }
-formatDate();
+console.log(process.argv);
+formatDate(process.argv[2], process.argv[3], process.argv[4]);
