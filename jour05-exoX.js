ef5cf0066f3543a073a24252ffe27ea45e55aa64
unknown <hassandahab11@gmail.com>
2020-12-16 08:53:05 +0100

    the last version of my tst j5

--- jour05-exoX.js
+++ jour05-exoX.js
@@ -13,10 +13,13 @@ console.log(calculate(process.argv[2], process.argv[3],process.argv[4]));
 
 //02 table
 
-var multiply = require("./table-utils");
+var{multiply, addition} = require("./utils/table-utils");
 
 multiply();
 console.log(process.argv);
 multiply(process.argv[2]);
 
-
+//03
+addition();
+console.log(process.argv);
+addition(process.argv[0]);
