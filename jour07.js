7f9b2eb1b927696e198315e2927582a5fddde5c4
unknown <hassandahab11@gmail.com>
2020-12-17 16:29:17 +0100

    third version of j7 test

--- jour07.js
+++ jour07.js
@@ -1,7 +1,23 @@
 // 01 Alphabet
 
-function sortLetters(xStriing) {
- var y =xStriing.split('').sort().join('');
- console.log(y);
+// function sortLetters(xStriing) {
+//  var y = xStriing.split('').sort().join('');
+//  console.log(y);
+//  console.log(xStriing);
+// }
+// sortLetters("konexio");
+
+
+//02 XOXO
+
+function countEach(xString){
+var y = xString;
+for(var i=0; i<=y.length; i++){
+
+if( y ="x" > "o"){
+    console.log("true");
+}else{
+    console.log("false");
 }
-sortLetters("konexio");
+} }
+countEach("xox");
