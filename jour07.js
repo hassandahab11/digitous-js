dc4e5c50732ab50d298197e53074d6f0caeba0fd
unknown <hassandahab11@gmail.com>
2021-01-04 15:34:38 +0100

    second version of tst j07

--- jour07.js
+++ jour07.js
@@ -5,3 +5,11 @@ fs.readFile("./jour07.txt", function(error, data){
     console.log(data.toString());
 })
 
+// 02 Map Double
+
+var numbers = [1, 2, 3, 4, 5];
+var doubles = numbers.map(function(num) {
+  return num * 2;
+});
+console.log(doubles);
+
